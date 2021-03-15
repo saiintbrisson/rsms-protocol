8522cda617581681c34b1c18d4b75038fbad3999
Luiz Carlos Mourão Paes de Carvalho <luizcarlosmpc@gmail.com>
2021-03-15 17:11:27 -0300

    feat: added support for bools

---
+++ protocol_internal/src/bool.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+use byteorder::{ReadBytesExt, WriteBytesExt};
+
+use crate::ProtocolSupport;
+
+impl ProtocolSupport for bool {
+    fn calculate_len(&self) -> usize {
+        1
+    }
+
+    fn deserialize<R: std::io::Read>(src: &mut R) -> std::io::Result<Self> {
+        Ok(match src.read_u8()? {
+            0 => false,
+            1 => true,
+            _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid bool value"))?
+        })
+    }
+
+    fn serialize<W: std::io::Write>(&self, dst: &mut W) -> std::io::Result<()> {
+        dst.write_u8(if *self { 1 } else { 0 })
+    }
+}
--- protocol_internal/src/lib.rs
+++ protocol_internal/src/lib.rs
@@ -1,3 +1,4 @@
+mod bool;
 mod dyn_array;
 mod numeral {
     mod int;
