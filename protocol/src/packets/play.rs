8fb501570258457fc781c7fdfef1fef6228eaefd
Luiz Carlos Mourão Paes de Carvalho <luizcarlosmpc@gmail.com>
2021-03-16 11:13:01 -0300

    feat: added keepalive, disconnect and clientsettings packets

--- protocol/src/packets/play.rs
+++ protocol/src/packets/play.rs
@@ -1,6 +1,13 @@
 #[protocol_derive::packets("ClientBound")]
 pub mod client_bound {
     use misc::prelude::{Difficulty, Dimension, GameMode};
+    
+    #[derive(Debug, Default, protocol_derive::ProtocolSupport)]
+    #[packet(0x00)]
+    pub struct KeepAlive {
+        #[protocol_field(varnum)]
+        pub keep_alive_id: i32,
+    }
 
     #[derive(Debug, Default, protocol_derive::ProtocolSupport)]
     #[packet(0x01)]
@@ -12,6 +19,34 @@ pub mod client_bound {
         pub max_players: u8,
         pub level_type: String,
         pub reduced_debug_info: bool,
+    }
+
+    #[derive(Debug, Default, protocol_derive::ProtocolSupport)]
+    #[packet(0x40)]
+    pub struct Disconnect {
+        pub reason: String,
+    }
+}
+
+#[protocol_derive::packets("ServerBound")]
+pub mod server_bound {
+    use misc::prelude::ChatMode;
+
+    #[derive(Debug, Default, protocol_derive::ProtocolSupport)]
+    #[packet(0x00)]
+    pub struct KeepAlive {
+        #[protocol_field(varnum)]
+        pub keep_alive_id: i32,
+    }
+
+    #[derive(Debug, Default, protocol_derive::ProtocolSupport)]
+    #[packet(0x15)]
+    pub struct ClientSettings {
+        pub locale: String,
+        pub view_distance: i8,
+        pub chat_mode: ChatMode,
+        pub chat_colors: bool,
+        pub displayed_skin_parts: u8,
     }
 }
 
